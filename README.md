547f16d80deca67e580a3719d38b68afa7533963
Kai <kaidronser@gmail.com>
2024-05-30 19:19:26 +0530

    working on function to separate values and operators

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Calculator-ver-0.0-
 This is my first project. I am making a calculator in C.
 Made the first code to calculate basic mathematical operations
-Making a code to take the input in a single line (unlinke in previous code where it took input in different line ) and calculate
+Made a code to take the input in a single line (unlinke in previous code where it took input in different line ) and calculate
+Also Made progress to put the values and operators in different array to solve program using bodmas
+have to improve and correct ver_0-02 
--- ver_0-02.c
+++ ver_0-02.c
@@ -1,8 +1,30 @@
 #include<stdio.h>
+#include<string.h>
+
+void filtration(char* exprsn,int* vv, char* ss){
+    while (*exprsn != '\0')
+    {
+        if (*exprsn=='+' || *exprsn=='-' || *exprsn=='*' || *exprsn=='/' || *exprsn=='(' || *exprsn==')' || *exprsn=='[' || *exprsn==']' || *exprsn=='{' || *exprsn=='}')
+        {
+            *ss=*exprsn;
+        }else if (*exprsn>=48 && *exprsn<=57)
+        {
+            *vv=*exprsn;
+            exprsn++;
+        }else continue;
+        exprsn++;
+    }
+}
+
+
 int main(int argc, char const *argv[])
 {
     char input[200];
+    int values[50];
+    char sign[50];
     scanf("%s",&input);
-    printf("%s",input);
+    printf("%s and the length is ",input);
+    filtration(input,values,sign);
+    printf("%s and nigga %s",values,sign);
     return 0;
 }
