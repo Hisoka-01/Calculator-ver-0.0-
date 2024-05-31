02246d2a4facb4db1601153d150cda1c4e15443e
Kai <kaidronser@gmail.com>
2024-05-31 22:06:48 +0530

    Wrote code to take input as string and sepearate them

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Calculator-ver-0.0-
-This is my first project. I am making a calculator in C <br/>.
-Made the first code to calculate basic mathematical operations
-Made a code to take the input in a single line (unlinke in previous code where it took input in different line ) and calculate
-Also Made progress to put the values and operators in different array to solve program using bodmas
-have to improve and correct ver_0-02 
+This is my first project. I am making a calculator in C .<br/>
+Made the first code to calculate basic mathematical operations.<br/>
+Made a code to take the input in a single line (unlinke in previous code where it took input in different line ) and do mathematical operation. <br/>
+Started working on solving equation by using BODMAS.<br/>
+Completed writing a code to accept input as string and seperate into two diff arrays to store values and signs.
--- ver_0-02.c
+++ ver_0-02.c
@@ -1,30 +1,61 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
-void filtration(char* exprsn,int* vv, char* ss){
+// add comments
+
+int filtration(char* exprsn, double* vv, char* ss){
+    char* num=(char*)calloc(12,sizeof(char));
+    char* err_ptr;
+    int i=0,k=0;
     while (*exprsn != '\0')
     {
         if (*exprsn=='+' || *exprsn=='-' || *exprsn=='*' || *exprsn=='/' || *exprsn=='(' || *exprsn==')' || *exprsn=='[' || *exprsn==']' || *exprsn=='{' || *exprsn=='}')
         {
             *ss=*exprsn;
-        }else if (*exprsn>=48 && *exprsn<=57)
-        {
-            *vv=*exprsn;
+            if(i!=0){
+            *vv=strtod(num,&err_ptr);
+            i=0;
+            free(num);
+            char* num=(char*)calloc(12,sizeof(char));
+            vv++;
+            k++;
+            }
+            ss++;
             exprsn++;
-        }else continue;
-        exprsn++;
+        }else if (*exprsn>=48 && *exprsn<=57 || *exprsn=='.')
+        {
+            num[i]=*exprsn;
+            exprsn++;
+            i++;
+        }else printf("\nUnknown character detected\n");
     }
+    exprsn--;
+    if (*exprsn>=48 && *exprsn<=57 )
+    {
+        *vv=strtod(num,&err_ptr);
+        k++;
+    }
+    free(num);
+    return k;
 }
 
 
 int main(int argc, char const *argv[])
 {
-    char input[200];
-    int values[50];
-    char sign[50];
+    char input[200]="";
+    double values[50];
+    char sign[50]="";
     scanf("%s",&input);
-    printf("%s and the length is ",input);
-    filtration(input,values,sign);
-    printf("%s and nigga %s",values,sign);
+    int len=filtration(input,values,sign);
+    for(int i=0;i<len;i++){
+        printf("%f ",values[i]);
+    }
+    printf("\n Above are the values given and sign length is %d\n",strlen(sign));
+        for(int i=0; i<strlen(sign); i++){
+        printf("%c ",sign[i]);
+    }
+    printf("\n Above are the signs given");
+
     return 0;
 }
