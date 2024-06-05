2a48669c4bc41eaf37986fea1632032172c8e094
Kai <kaidronser@gmail.com>
2024-06-05 14:49:42 +0530

    Wrote a code to accept and filter the input

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Made the first code to calculate basic mathematical operations.<br/>
 Made a code to take the input in a single line (unlinke in previous code where it took input in different line ) and do mathematical operation. <br/>
 Started working on solving equation by using BODMAS.<br/>
 Completed writing a code to accept input as string and seperate into two diff arrays to store values and signs.
+Test run is not done yet
--- ver_0-02.c
+++ ver_0-02.c
@@ -4,20 +4,20 @@
 
 // add comments
 
-int filtration(char* exprsn, double* vv, char* ss){
-    char* num=(char*)calloc(12,sizeof(char));
-    char* err_ptr;
-    int i=0,k=0;
-    while (*exprsn != '\0')
+int filtration(char* exprsn, double* vv, char* ss){       //fucntion to seperate values and signs in to two diff arrays
+    char* num=(char*)calloc(12,sizeof(char));             //made an array to turn strings into integers 
+    char* err_ptr;                                        //pointer for strod function
+    int i=0,k=0;                                          //counter for length of number string and length of the array of values
+    while (*exprsn !='\0')
     {
         if (*exprsn=='+' || *exprsn=='-' || *exprsn=='*' || *exprsn=='/' || *exprsn=='(' || *exprsn==')' || *exprsn=='[' || *exprsn==']' || *exprsn=='{' || *exprsn=='}')
         {
-            *ss=*exprsn;
-            if(i!=0){
-            *vv=strtod(num,&err_ptr);
+            *ss=*exprsn;                                   //taking in the sign
+            if(i!=0){                                      //chekcing if there is any number in string
+            *vv=strtod(num,&err_ptr);                      //converting the string into integer
             i=0;
-            free(num);
-            char* num=(char*)calloc(12,sizeof(char));
+            free(num);                                     // frred the dynamic memory
+            char* num=(char*)calloc(12,sizeof(char));      //using dynamic memory allocation
             vv++;
             k++;
             }
@@ -25,30 +25,49 @@ int filtration(char* exprsn, double* vv, char* ss){
             exprsn++;
         }else if (*exprsn>=48 && *exprsn<=57 || *exprsn=='.')
         {
-            num[i]=*exprsn;
+            num[i]=*exprsn;                                //taking the characters of the integers in a string to turn it into an integer
             exprsn++;
             i++;
-        }else printf("\nUnknown character detected\n");
+        }else if ( *exprsn==' ' &&  i!=0)                 //checking for space in between the numbers or signs
+        {
+            *vv=strtod(num,&err_ptr);
+            i=0;
+            free(num);
+            char* num=(char*)calloc(12,sizeof(char));
+            vv++;
+            k++;
+            
+        }else 
+        {
+            if ( *exprsn!=' ')
+            {
+                printf("\nUnknown character detected\n");
+            }
+            exprsn++;
+        }
     }
     exprsn--;
-    if (*exprsn>=48 && *exprsn<=57 )
+    if (*exprsn>=48 && *exprsn<=57 )                       //checking if a number is left in the string
     {
         *vv=strtod(num,&err_ptr);
         k++;
     }
-    free(num);
+    free(num);                                             //freed the dynamic memory
     return k;
 }
 
 
 int main(int argc, char const *argv[])
 {
-    char input[200]="";
+    char input[200]="";                                   //variable decleration
     double values[50];
     char sign[50]="";
-    scanf("%s",&input);
-    int len=filtration(input,values,sign);
-    for(int i=0;i<len;i++){
+    scanf("%[^\n]%*c",&input);                            //accepting input from the user
+    int len=filtration(input,values,sign);                // using filteration function to seperate the values and signs and also find the length of values array
+  
+  //testing if function is taking value properly or not
+
+  for(int i=0;i<len;i++){
         printf("%f ",values[i]);
     }
     printf("\n Above are the values given and sign length is %d\n",strlen(sign));
